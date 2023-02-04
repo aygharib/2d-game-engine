31e3c01dc61db63b357c29f65b5be3de2952adb6
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-02-03 22:14:45 -0500

    Add Vec2

---
+++ src/Vec2.cpp
@@ -0,0 +1,49 @@
+#include "Vec2.h"
+
+Vec2::Vec2(float x, float y) 
+    : x{x}, y{y}
+{}
+
+auto Vec2::operator == (const Vec2& rhs) const -> bool {
+    return x == rhs.x && y == rhs.y;
+}
+
+auto Vec2::operator != (const Vec2& rhs) const -> bool {
+    return x != rhs.x || y != rhs.y;
+}
+
+auto Vec2::operator + (const Vec2& rhs) const -> Vec2 {
+    return Vec2{x + rhs.x, y + rhs.y};
+}
+
+auto Vec2::operator - (const Vec2& rhs) const -> Vec2 {
+    return Vec2{x - rhs.x, y - rhs.y};
+}
+
+auto Vec2::operator * (const float value) const -> Vec2 {
+    return Vec2{x * value, y * value};
+}
+
+auto Vec2::operator / (const float value) const -> Vec2 {
+    return Vec2{x / value, y / value};
+}
+
+auto Vec2::operator += (const Vec2& rhs) -> void {
+    x += rhs.x;
+    y += rhs.y;
+}
+
+auto Vec2::operator -= (const Vec2& rhs) -> void {
+    x -= rhs.x;
+    x -= rhs.y;
+}
+
+auto Vec2::operator *= (const float value) -> void {
+    x *= value;
+    y *= value;
+}
+
+auto Vec2::operator /= (const float value) -> void {
+    x /= value;
+    y /= value;
+}
---
+++ src/Vec2.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Vec2 {
+public:
+    float x = 0.F;
+    float y = 0.F;
+
+    Vec2() = default;
+    Vec2(float x, float y);
+
+    auto operator == (const Vec2& rhs) const -> bool;
+    auto operator != (const Vec2& rhs) const -> bool;
+
+    auto operator + (const Vec2& rhs)  const -> Vec2;
+    auto operator - (const Vec2& rhs)  const -> Vec2;
+    auto operator * (float value)      const -> Vec2;
+    auto operator / (float value)      const -> Vec2;
+
+    auto operator += (const Vec2& rhs) -> void;
+    auto operator -= (const Vec2& rhs) -> void;
+    auto operator *= (float value)     -> void;
+    auto operator /= (float value)     -> void;
+
+    [[nodiscard]] auto dist(const Vec2& rhs) const -> float;
+    [[nodiscard]] auto length()              const -> float;
+    [[nodiscard]] auto normalize()           const -> Vec2;
+};
