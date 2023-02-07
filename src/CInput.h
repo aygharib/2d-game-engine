8f879426f4c826fd95b9bcc65d579817c3957fc6
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-02-07 08:09:21 -0500

    Update CInput to use Component

--- src/CInput.h
+++ src/CInput.h
@@ -1,11 +1,14 @@
 #pragma once
 
-class CInput {
+#include "Component.h"
+
+class CInput : public Component {
 public:
-    bool up    = false;
-    bool down  = false;
-    bool left  = false;
-    bool right = false;
+    int x;
+    bool up   {false};
+    bool down {false};
+    bool left {false};
+    bool right{false};
 
     CInput() = default;
 };
